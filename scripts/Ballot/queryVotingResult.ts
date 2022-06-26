3ea1877acc435ac97fdb1d45e7ccb9b680d0bf91
Vid <vid@meisterlabs.com>
2022-06-26 10:20:30 +0200

    first commit

---
+++ scripts/Ballot/queryVotingResult.ts
@@ -0,0 +1,45 @@
+import { Contract, ethers } from "ethers";
+import "dotenv/config";
+import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json";
+// eslint-disable-next-line node/no-missing-import
+import { Ballot } from "../../typechain/index";
+
+// This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
+// Do never expose your keys like this
+const EXPOSED_KEY =
+  "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
+
+async function main() {
+  const wallet =
+    process.env.MNEMONIC && process.env.MNEMONIC.length > 0
+      ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
+      : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+  const provider = ethers.providers.getDefaultProvider("ropsten");
+  const signer = wallet.connect(provider);
+  const balanceBN = await signer.getBalance();
+  const balance = Number(ethers.utils.formatEther(balanceBN));
+  if (balance < 0.01) {
+    throw new Error("Not enough ether");
+  }
+  if (process.argv.length < 3) throw new Error("Ballot address missing");
+  const ballotAddress = process.argv[2];
+  if (process.argv.length < 4)
+    throw new Error("Index of proposal's voting result missing");
+  const index = process.argv[3];
+
+  const ballotContract: Ballot = new Contract(
+    ballotAddress,
+    ballotJson.abi,
+    signer
+  ) as Ballot;
+
+  const proposal = await ballotContract.proposals(index);
+
+  const votingResult = proposal.voteCount.toNumber();
+  console.log("votingResult", votingResult);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
