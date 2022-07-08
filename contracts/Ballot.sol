d3622863b1d41114774dd5d43c330a9ed8857571
Lucien Akchoté <l.akchote@gmail.com>
2022-07-08 09:05:49 +0200

    Add NatSpec

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -2,61 +2,76 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 /// @title Voting with delegation.
+/// @author Encode June Bootcamp Participants
+/// @notice You can use this contract to vote on proposals, delegate and show the winning proposal
+/// @dev This is a simple ballot contract intended to show what needs to be implemented to make it work on a basic level
+/// @custom:encode This is intended to teach Solidity basics for first lesson
 contract Ballot {
-    // This declares a new complex type which will
-    // be used for variables later.
-    // It will represent a single voter.
+    /**
+    * @dev This declares a new complex type which will be used for variables later.
+    * It will represent a single voter.
+    */
     struct Voter {
-        uint256 weight; // weight is accumulated by delegation
-        bool voted; // if true, that person already voted
-        address delegate; // person delegated to
-        uint256 vote; // index of the voted proposal
+        uint256 weight; /// @dev weight is accumulated by delegation
+        bool voted; /// @dev if true, that person already voted
+        address delegate; /// @dev person delegated to
+        uint256 vote; /// @dev index of the voted proposal
     }
 
-    // This is a type for a single proposal.
+    /// @dev This is a type for a single proposal.
     struct Proposal {
-        bytes32 name; // short name (up to 32 bytes)
-        uint256 voteCount; // number of accumulated votes
+        /// @dev short name (up to 32 bytes)
+        bytes32 name; 
+        /// @dev number of accumulated votes
+        uint256 voteCount; 
     }
 
+    /// @dev Stores the `chairperson` address which will be able to give right to vote on proposals.
     address public chairperson;
 
-    // This declares a state variable that
-    // stores a `Voter` struct for each possible address.
+     
+    /// @dev This declares a state variable that stores a `Voter` struct for each possible address.
     mapping(address => Voter) public voters;
 
-    // A dynamically-sized array of `Proposal` structs.
+    /// @dev A dynamically-sized array of `Proposal` structs.
     Proposal[] public proposals;
 
-    /// Create a new ballot to choose one of `proposalNames`.
+    /// @dev Create a new ballot to choose one of `proposalNames`.
+    /// @param proposalNames An array of proposals to vote on
     constructor(bytes32[] memory proposalNames) {
         chairperson = msg.sender;
         voters[chairperson].weight = 1;
-
-        // For each of the provided proposal names,
-        // create a new proposal object and add it
-        // to the end of the array.
+        /**
+        * @dev For each of the provided proposal names,
+        * create a new proposal object and add it
+        * to the end of the array.
+        */
         for (uint256 i = 0; i < proposalNames.length; i++) {
-            // `Proposal({...})` creates a temporary
-            // Proposal object and `proposals.push(...)`
-            // appends it to the end of `proposals`.
+            /**
+            * @dev `Proposal({...})` creates a temporary
+            * Proposal object and `proposals.push(...)`
+            * appends it to the end of `proposals`.
+            */
             proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
         }
     }
 
-    // Give `voter` the right to vote on this ballot.
-    // May only be called by `chairperson`.
+    /// @notice Give `voter` the right to vote on this ballot.
+    /// @dev May only be called by `chairperson`.
+    /// @param voter The address to give right to vote on proposals
     function giveRightToVote(address voter) external {
-        // If the first argument of `require` evaluates
-        // to `false`, execution terminates and all
-        // changes to the state and to Ether balances
-        // are reverted.
-        // This used to consume all gas in old EVM versions, but
-        // not anymore.
-        // It is often a good idea to use `require` to check if
-        // functions are called correctly.
-        // As a second argument, you can also provide an
-        // explanation about what went wrong.
+        /** 
+        * @dev If the first argument of `require` evaluates
+        * to `false`, execution terminates and all
+        * changes to the state and to Ether balances
+        * are reverted.
+        * This used to consume all gas in old EVM versions, but
+        * not anymore.
+        * It is often a good idea to use `require` to check if
+        * functions are called correctly.
+        * As a second argument, you can also provide an
+        * explanation about what went wrong.
+        */
         require(
             msg.sender == chairperson,
             "Only chairperson can give right to vote."
@@ -66,50 +81,61 @@ contract Ballot {
         voters[voter].weight = 1;
     }
 
-    /// Delegate your vote to the voter `to`.
+    /// @notice Delegate your vote to the voter `to`.
     function delegate(address to) external {
-        // assigns reference
+        /// @dev assigns reference
         Voter storage sender = voters[msg.sender];
         require(!sender.voted, "You already voted.");
 
         require(to != msg.sender, "Self-delegation is disallowed.");
 
-        // Forward the delegation as long as
-        // `to` also delegated.
-        // In general, such loops are very dangerous,
-        // because if they run too long, they might
-        // need more gas than is available in a block.
-        // In this case, the delegation will not be executed,
-        // but in other situations, such loops might
-        // cause a contract to get "stuck" completely.
+        /**
+        * @dev Forward the delegation as long as
+        * `to` also delegated.
+        * In general, such loops are very dangerous,
+        * because if they run too long, they might
+        * need more gas than is available in a block.
+        * In this case, the delegation will not be executed,
+        * but in other situations, such loops might
+        * cause a contract to get "stuck" completely.
+        */
         while (voters[to].delegate != address(0)) {
             to = voters[to].delegate;
 
-            // We found a loop in the delegation, not allowed.
+            /// @dev We found a loop in the delegation, not allowed.
             require(to != msg.sender, "Found loop in delegation.");
         }
-
-        // Since `sender` is a reference, this
-        // modifies `voters[msg.sender].voted`
+        
+        /**
+        * @dev Since `sender` is a reference, this
+        * modifies `voters[msg.sender].voted`
+        */
         Voter storage delegate_ = voters[to];
 
-        // Voters cannot delegate to wallets that cannot vote.
+        /// @dev Voters cannot delegate to wallets that cannot vote.
         require(delegate_.weight >= 1);
         sender.voted = true;
         sender.delegate = to;
         if (delegate_.voted) {
-            // If the delegate already voted,
-            // directly add to the number of votes
+            /**
+            * @dev If the delegate already voted,
+            * directly add to the number of votes
+            */
             proposals[delegate_.vote].voteCount += sender.weight;
         } else {
-            // If the delegate did not vote yet,
-            // add to her weight.
+            /**
+            * @dev If the delegate did not vote yet,
+            * add to her weight.
+            */
             delegate_.weight += sender.weight;
         }
     }
 
-    /// Give your vote (including votes delegated to you)
-    /// to proposal `proposals[proposal].name`.
+    /**
+    * @notice Give your vote (including votes delegated to you)
+    * to proposal `proposals[proposal].name`.
+    */
+    /// @param proposal The proposal index to vote on
     function vote(uint256 proposal) external {
         Voter storage sender = voters[msg.sender];
         require(sender.weight != 0, "Has no right to vote");
@@ -117,14 +143,20 @@ contract Ballot {
         sender.voted = true;
         sender.vote = proposal;
 
-        // If `proposal` is out of the range of the array,
-        // this will throw automatically and revert all
-        // changes.
+        /**
+        * @dev If `proposal` is out of the range of the array,
+        * this will throw automatically and revert all
+        * changes.
+        */
         proposals[proposal].voteCount += sender.weight;
     }
 
-    /// @dev Computes the winning proposal taking all
-    /// previous votes into account.
+    /**
+    * @dev Computes the winning proposal taking all
+    * previous votes into account.
+    */
+    /// @notice Retrieves the index of the winning proposal
+    /// @return winningProposal_ The winning proposal index
     function winningProposal() public view returns (uint256 winningProposal_) {
         uint256 winningVoteCount = 0;
         for (uint256 p = 0; p < proposals.length; p++) {
@@ -135,9 +167,14 @@ contract Ballot {
         }
     }
 
-    // Calls winningProposal() function to get the index
-    // of the winner contained in the proposals array and then
-    // returns the name of the winner
+
+    /// @notice Gives the name of the winning proposal
+    /**
+    * @dev Calls winningProposal() function to get the index
+    * of the winner contained in the proposals array and then
+    * returns the name of the winner
+    */
+    /// @return winnerName_ The winning proposal name
     function winnerName() external view returns (bytes32 winnerName_) {
         winnerName_ = proposals[winningProposal()].name;
     }
